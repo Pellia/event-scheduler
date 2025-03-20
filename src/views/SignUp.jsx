b014a7e2de5b955e37487b1499eb62b72bcee0a9
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-03-20 16:59:43 +0100

    added base sign up component

--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -1,25 +1,46 @@
-import Navbar from "../components/Navbar";
+import { useState } from "react";
 
 const SingUp = () => {
-    // Send a post request with a header and a body
-    const handleCreateUser = () => {
-        // post request goes here
+    const [inputValue, setInputValue] = useState(null || { email: "", password: "" });
+
+    const handleChange = (e) => {
+        setInputValue((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+        // console.log(inputValue);
+    };
+
+    const handleCreateUser = (e) => {
+        e.preventDefault();
+        // console.log(inputValue["email"]);
+        // console.log(inputValue["password"]);
+        // console.log(e.target[0].value);
+        // console.log(e.target[1].value);
+
+        // const email = e.target[0].value;
+        // const password = e.target[0].value;
         fetch("http://localhost:3001/api/users", {
             method: "POST",
             headers: {
                 "Content-Type": "application/JSON",
             },
             body: JSON.stringify({
-                email: "user2@example.com",
-                password: "password123",
+                email: inputValue["email"],
+                password: inputValue["password"],
             }),
-        });
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data));
     };
 
     return (
         <div>
             <h1>Sign Up</h1>
-            <button onClick={handleCreateUser}>Submit Create User</button>
+            <form onSubmit={handleCreateUser}>
+                <label htmlFor="email">Email</label>
+                <input onChange={handleChange} className="border" type="email" name="email" id="email" required />
+                <label htmlFor="password">Password</label>
+                <input onChange={handleChange} className="border" type="password" name="password" id="password" required />
+                <button className="bg-blue-200 p-2">Submit Create User</button>
+            </form>
         </div>
     );
 };
