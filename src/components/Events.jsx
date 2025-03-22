a89ec33632585872202937ada1a25bef2ce0a81e
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-03-22 10:08:35 +0100

    clean up event component

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,4 +1,7 @@
+// Hooks
 import { useState, useEffect } from "react";
+
+// React Router
 import { Link } from "react-router-dom";
 
 const Events = () => {
@@ -8,20 +11,29 @@ const Events = () => {
         fetch("http://localhost:3001/api/events")
             .then((res) => res.json())
             .then((data) => {
-                // console.log(data.results);
                 setEvents(data.results);
             });
     }, []);
 
     return (
-        <div>
+        <div className="grid grid-cols-1 gap-2 md:grid-cols-3 sm:grid-cols-2 ">
             {events &&
                 events.map((item, index) => {
                     return (
-                        <div className="border" key={index}>
-                            <div>{item.title}</div>
-                            <div>{item.description}</div>
-                            <Link to={`events/${index + 1}`}>Details</Link>
+                        <div className="border border-neutral-200 bg-white p-4 rounded-lg flex flex-col gap-4" key={index}>
+                            <div className="flex justify-between items-center">
+                                <h2 className="text-xl font-bold">{item.title}</h2>
+                                <p>{item.date.slice(0, 10)}</p>
+                            </div>
+                            <hr className="border-neutral-400 border-dashed" />
+                            <p>{item.description}</p>
+                            <hr className="border-neutral-400 border-dashed" />
+                            <div className="flex justify-between items-center">
+                                <p>{item.location}</p>
+                                <Link className=" bg-pink-200 p-2 rounded-md text-sm hover:bg-violet-200" to={`events/${item.id}`}>
+                                    More Details
+                                </Link>
+                            </div>
                         </div>
                     );
                 })}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,13 @@
-import { useState, useEffect } from "react";
+// Components
 import Events from "../components/Events";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Home</h1>
-            <Events />
+        <div className="p-4">
+            <div className="container mx-auto flex flex-col items-center gap-4">
+                <h1 className="text-neutral-800 font-bold text-2xl text-center">Event Overview</h1>
+                <Events />
+            </div>
         </div>
     );
 };
